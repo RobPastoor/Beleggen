485f7d32b3ab6c31687cb8e650771e0068485458
RobPastoor <rob.pastoor@telfort.nl>
2016-12-31 21:50:18 +0100

    2016-12-31

---
+++ BeleggenOphalenData.R
@@ -0,0 +1,29 @@
+library(XML)
+library(tidyr)
+library(foreign)
+library(dplyr)
+
+fondsen <- c("nn.bas.mat",
+             "nn.dutch",
+             "nn.dmixf.1",
+             "nn.eu.smcp",
+             "nn.g.obl.f",
+             "nn.it.fd",
+             "nn.prem.dv",
+             "nn.telcoms"
+)
+
+for(i in 1:length(fondsen)) {
+  fonds <- fondsen[i]
+  rDF <- read.dbf(paste("Data\\", fonds, ".dbf", sep = ""), as.is = TRUE)
+  fileUrl <- paste("http://www.behr.nl/Beurs/Slotkoersen/slotkoersen.php?fd=", fonds, sep = "")
+  doc <- htmlTreeParse(fileUrl, useInternal = TRUE)
+  koers <- xpathSApply(doc, "//pre", xmlValue)
+  sKoers <- unlist(strsplit(koers, split = "\n"))
+  wDF <- data.frame(datumkoers = unlist(sKoers), stringsAsFactors = FALSE)
+  rbDF <- rbind(rDF, wDF)
+  oDF <- rbDF %>% distinct(datumkoers, .keep_all = TRUE)
+  write.dbf(oDF, paste("Data\\", fonds, ".dbf", sep = ""), factor2char = TRUE)
+  rm("oDF", "rbDF", "rDF", "wDF", "doc", "fileUrl", "fonds", "koers", "sKoers")
+}
+rm("fondsen", "i")
---
+++ BeleggenTonenDataSavePNG.R
@@ -0,0 +1,46 @@
+library(XML)
+library(tidyr)
+library(foreign)
+library(dplyr)
+
+fondsen <- c("nn.bas.mat",
+             "nn.dutch",
+             "nn.dmixf.1",
+             "nn.eu.smcp",
+             "nn.g.obl.f",
+             "nn.it.fd",
+             "nn.prem.dv",
+             "nn.telcoms"
+)
+
+for(i in 1:length(fondsen)) {
+  fonds <- fondsen[i]
+  rDF <- read.dbf(paste("Data\\", fonds, ".dbf", sep = ""), as.is = TRUE)
+  rDF["datum"] <- paste("20", substring(rDF[,1], 1, 6), sep="")
+  rDF["koers"] <- as.numeric(substring(rDF[,1], 8, 15))
+  rDF["periode"] <- paste("20", substring(rDF[,1], 1, 2), "-",substring(rDF[,1], 3, 4), sep="")
+  uMaand <- unique(rDF$periode
+  )
+  fMaand <- factor(rDF$periode,
+                   levels = c(uMaand)
+  )
+  png(file = paste("PNG\\", fonds, "_", Sys.Date(), ".png", sep = ""), width = 1500, height = 828)
+  boxplot(koers~fMaand,
+          data=rDF,
+          main=toupper(fonds),
+          font.main=3,
+          cex.main=1,
+          xlab="Maand",
+          las=1,
+          #ylim=c(40.0, 70.0), 
+          ylab="€",
+          font.lab=3,
+          col="yellow",
+          outline = TRUE
+  )
+  mtext(Sys.Date(), 3, cex=0.8, line = 0)
+  mtext("Koers in euro's", 2, cex=0.8, line = 2)
+  dev.off()
+  rm("rDF", "fonds", "fMaand", "uMaand")
+}
+rm("fondsen", "i")
